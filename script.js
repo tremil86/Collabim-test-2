353f6676ca8f5784aaff7be5f18f5dcb8750e255
tremil86 <milos86tregl@gmail.com>
2023-04-11 10:12:29 +0200

    Update script.js
    needed

--- script.js
+++ script.js
@@ -1,33 +1,50 @@
-document.getElementById('searchForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var searchQuery = document.getElementById('searchInput').value;
-    var searchUrl = 'https://www.google.com/search?q=' + encodeURIComponent(searchQuery);
+// Import Cheerio na začátku souboru
+const cheerio = require('cheerio');
 
-    fetch(searchUrl)
-    .then(response => response.text())
-    .then(data => {
-        var searchResults = parseSearchResults(data);
-        downloadSearchResults(searchResults);
-    })
-    .catch(error => console.error(error));
-});
-
+// Přesunutí definice funkce parseSearchResults na začátek souboru
 function parseSearchResults(html) {
-    // Zde implementujte kód pro extrakci výsledků z Google vyhledávání
-    // Např. můžete použít knihovnu Cheerio nebo provést parsování pomocí regulárních výrazů
-    // Výsledky uložte do vhodné datové struktury (např. pole nebo objektu)
-    // a vraťte tuto datovou strukturu
+  // Načtení HTML pomocí Cheerio
+  const $ = cheerio.load(html);
+
+  // Extrakce výsledků z Google vyhledávání
+  const results = [];
+  $('div.g').each((index, element) => {
+    const titleElement = $(element).find('h3');
+    const title = titleElement.text().trim();
+    const linkElement = titleElement.find('a');
+    const link = linkElement.attr('href');
+    results.push({ title, link });
+  });
+
+  // Vrácení datové struktury s výsledky
+  return results;
 }
 
+// Upravený event listener pro událost submit na formuláři
+document.getElementById('searchForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  var searchQuery = document.getElementById('searchInput').value;
+  var searchUrl = 'https://www.google.com/search?q=' + encodeURIComponent(searchQuery);
+
+  fetch(searchUrl)
+  .then(response => response.text())
+  .then(data => {
+    var searchResults = parseSearchResults(data);
+    downloadSearchResults(searchResults);
+  })
+  .catch(error => console.error(error));
+});
+
+// Funkce downloadSearchResults zůstává nezměněna
 function downloadSearchResults(results) {
-    var json = JSON.stringify(results);
-    var blob = new Blob([json], {type: 'application/json'});
-    var url = URL.createObjectURL(blob);
-    var link = document.createElement('a');
-    link.href = url;
-    link.download = 'search_results.json';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    URL.revokeObjectURL(url);
+  var json = JSON.stringify(results);
+  var blob = new Blob([json], {type: 'application/json'});
+  var url = URL.createObjectURL(blob);
+  var link = document.createElement('a');
+  link.href = url;
+  link.download = 'search_results.json';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
 }
