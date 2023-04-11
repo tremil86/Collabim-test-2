a40943275ac965a22b6066394df89c160ee47a70
tremil86 <milos86tregl@gmail.com>
2023-04-11 10:22:45 +0200

    Update script.js
    again failed, but never ending

--- script.js
+++ script.js
@@ -1,50 +1,40 @@
-// Import Cheerio na začátku souboru
-const cheerio = require('cheerio');
-
-// Přesunutí definice funkce parseSearchResults na začátek souboru
-function parseSearchResults(html) {
-  // Načtení HTML pomocí Cheerio
-  const $ = cheerio.load(html);
-
-  // Extrakce výsledků z Google vyhledávání
-  const results = [];
-  $('div.g').each((index, element) => {
-    const titleElement = $(element).find('h3');
-    const title = titleElement.text().trim();
-    const linkElement = titleElement.find('a');
-    const link = linkElement.attr('href');
-    results.push({ title, link });
-  });
-
-  // Vrácení datové struktury s výsledky
-  return results;
-}
-
-// Upravený event listener pro událost submit na formuláři
 document.getElementById('searchForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-  var searchQuery = document.getElementById('searchInput').value;
-  var searchUrl = 'https://www.google.com/search?q=' + encodeURIComponent(searchQuery);
+    event.preventDefault();
+    var searchQuery = document.getElementById('searchInput').value;
+    var searchUrl = 'https://www.google.com/search?q=' + encodeURIComponent(searchQuery);
 
-  fetch(searchUrl)
-  .then(response => response.text())
-  .then(data => {
-    var searchResults = parseSearchResults(data);
-    downloadSearchResults(searchResults);
-  })
-  .catch(error => console.error(error));
+    fetch(searchUrl)
+    .then(response => response.text())
+    .then(data => {
+        var searchResults = parseSearchResults(data);
+        downloadSearchResults(searchResults);
+    })
+    .catch(error => console.error(error));
 });
 
-// Funkce downloadSearchResults zůstává nezměněna
+function parseSearchResults(html) {
+    const cheerio = require('cheerio');
+    const $ = cheerio.load(html);
+    const results = [];
+    $('div.g').each((index, element) => {
+        const titleElement = $(element).find('h3');
+        const title = titleElement.text().trim();
+        const linkElement = titleElement.find('a');
+        const link = linkElement.attr('href');
+        results.push({ title, link });
+    });
+    return results;
+}
+
 function downloadSearchResults(results) {
-  var json = JSON.stringify(results);
-  var blob = new Blob([json], {type: 'application/json'});
-  var url = URL.createObjectURL(blob);
-  var link = document.createElement('a');
-  link.href = url;
-  link.download = 'search_results.json';
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-  URL.revokeObjectURL(url);
+    var json = JSON.stringify(results);
+    var blob = new Blob([json], {type: 'application/json'});
+    var url = URL.createObjectURL(blob);
+    var link = document.createElement('a');
+    link.href = url;
+    link.download = 'search_results.json';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
 }
